ec715259b02b31fda64e2b51da5b49f37d63d071
Mark Reid <mreid@mozilla.com>
2013-03-19 10:11:10 -0300

    Redirect to log and add error checking.

--- src/main/sh/daily-marketplace-cron.sh
+++ src/main/sh/daily-marketplace-cron.sh
@@ -3,11 +3,31 @@
 . $(dirname $0)/daily-marketplace-setvars.sh
 
 cd $ETL_HOME
-${PIG_HOME}/bin/pig -param date=$YESTERDAY -param marketplace=$MARKETPLACE $ETL_HOME/marketplace_daily_aggregates.pig
+LOG=$ETL_HOME/logs/daily-marketplace-cron.log
 
-hadoop fs -getmerge webapp_stats_date-$YESTERDAY $ETL_HOME/marketplace/webapp_stats_date-$YESTERDAY.txt
+${PIG_HOME}/bin/pig -param date=$YESTERDAY -param marketplace=$MARKETPLACE $ETL_HOME/marketplace_daily_aggregates.pig > $LOG 2>&1
+MARKET_RESULT=$?
+if [ "$MARKET_RESULT" -eq "0" ]; then
+   echo "Marketplace Stats export succeeded for $YESTERDAY"
+else
+   echo "ERROR: Marketplace Stats Export failed (code $MARKET_RESULT) for $YESTERDAY.  Check $LOG for more details."
+   exit 2
+fi
+
+hadoop fs -getmerge webapp_stats_date-$YESTERDAY $ETL_HOME/marketplace/webapp_stats_date-$YESTERDAY.txt >> $LOG 2>&1
 scp $ETL_HOME/marketplace/webapp_stats_date-$YESTERDAY.txt ${PUB_SERVER}:${PUB_PATH}/
-hadoop fs -getmerge webapp_stats_device-$YESTERDAY $ETL_HOME/marketplace/webapp_stats_device-$YESTERDAY.txt
+if [ ! "$?" -eq "0" ]; then
+   echo "ERROR: Failed to transfer webapp_stats_date-$YESTERDAY.txt to $PUB_SERVER"
+fi
+
+hadoop fs -getmerge webapp_stats_device-$YESTERDAY $ETL_HOME/marketplace/webapp_stats_device-$YESTERDAY.txt >> $LOG 2>&1
 scp $ETL_HOME/marketplace/webapp_stats_device-$YESTERDAY.txt ${PUB_SERVER}:${PUB_PATH}/
-hadoop fs -getmerge webapp_stats_source-$YESTERDAY $ETL_HOME/marketplace/webapp_stats_source-$YESTERDAY.txt
+if [ ! "$?" -eq "0" ]; then
+   echo "ERROR: Failed to transfer webapp_stats_device-$YESTERDAY.txt to $PUB_SERVER"
+fi
+
+hadoop fs -getmerge webapp_stats_source-$YESTERDAY $ETL_HOME/marketplace/webapp_stats_source-$YESTERDAY.txt >> $LOG 2>&1
 scp $ETL_HOME/marketplace/webapp_stats_source-$YESTERDAY.txt ${PUB_SERVER}:${PUB_PATH}/
+if [ ! "$?" -eq "0" ]; then
+   echo "ERROR: Failed to transfer webapp_stats_source-$YESTERDAY.txt to $PUB_SERVER"
+fi
