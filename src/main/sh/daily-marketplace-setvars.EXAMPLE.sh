eeb9c05f9bc4c4da7318ef8d03c97144f7ff1a9b
Mark Reid <mreid@mozilla.com>
2013-03-19 10:26:16 -0300

    Fix the date format

--- src/main/sh/daily-marketplace-setvars.EXAMPLE.sh
+++ src/main/sh/daily-marketplace-setvars.EXAMPLE.sh
@@ -5,11 +5,11 @@ PIG_HOME=/usr/lib/pig
 ETL_HOME=/home/etl
 
 ## Default to running for yesterday.
-YESTERDAY="`date +%Y%m%d --date="1 day ago"`"
+YESTERDAY="`date +%Y-%m-%d --date="1 day ago"`"
 
 ## If a parameter was passed in, use that for the target day:
 if [ ! -z "$1" ]; then
-  YESTERDAY=$(date -d "$1" +%Y%m%d)
+  YESTERDAY=$(date -d "$1" +%Y-%m-%d)
 fi
 
 MARKETPLACE=marketplace_example
